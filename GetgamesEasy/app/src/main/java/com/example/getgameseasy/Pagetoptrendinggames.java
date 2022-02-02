96eeb7d449cc491ea09e5b78550e788e970b1338
Raul1Gomes <RalgoTera@gmail.com>
2022-02-02 14:54:31 +0000

    update 2

--- GetgamesEasy/app/src/main/java/com/example/getgameseasy/Pagetoptrendinggames.java
+++ GetgamesEasy/app/src/main/java/com/example/getgameseasy/Pagetoptrendinggames.java
@@ -2,7 +2,9 @@ package com.example.getgameseasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Pagetoptrendinggames extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Pagetoptrendinggames extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagetoptrendinggames);
+    }
+
+    public void Voltarmain(View view) {
+        Intent intent=new Intent(this,MainPage.class);
+        startActivity(intent);
     }
 }
