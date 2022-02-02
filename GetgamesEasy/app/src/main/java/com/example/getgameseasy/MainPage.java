581c403625ed595c994f50e47263aaee919e7ea0
Raul1Gomes <RalgoTera@gmail.com>
2022-02-02 14:31:17 +0000

    upload

--- GetgamesEasy/app/src/main/java/com/example/getgameseasy/MainPage.java
+++ GetgamesEasy/app/src/main/java/com/example/getgameseasy/MainPage.java
@@ -2,7 +2,9 @@ package com.example.getgameseasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainPage extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+
+    public void Enviarparapagetop(View view) {
+        Intent intent=new Intent(this,Pagetoptrendinggames.class);
+        startActivity(intent);
+    }
+
+    public void Enviarpromotionpage(View view) {
+        Intent intent=new Intent(this,PromotionPage.class);
+        startActivity(intent);
     }
 }
